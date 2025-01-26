6593c5bf5b3b5d3402ff5e63ead353dbef3274e3
Yasin Silavi <ys.silavi@gmail.com>
2025-01-26 12:33:56 +0330

    feat: extend home feature routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { homeRoutes } from "./features/home/home.routes";
 
-export const routes: Routes = [];
+export const routes: Routes = [...homeRoutes];
